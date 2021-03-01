a742c87b431075e5062176f8a1f7697bde5fb159
Gabriel Mattos <gabriel.mattos17@hotmail.com>
2021-02-28 23:45:32 -0300

    Gerando 50 objetos na cena

---
+++ Assets/_Project/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private GameObject objectPrefab;
+    [SerializeField] private int numberOfObjectsInScene;
+    [SerializeField] private Transform parentObjects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenenateObjectsInScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenenateObjectsInScene()
+    {
+        for(int i = 0; i < numberOfObjectsInScene; i++)
+        {
+            GameObject GO = Instantiate(objectPrefab, transform.position, Quaternion.identity);
+            GO.transform.parent = parentObjects;
+        }
+    }
+}
